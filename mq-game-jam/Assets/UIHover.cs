718759695215ff73cc36198bf40e57d3b4759830
Mark Saba <79881241+marksabamq@users.noreply.github.com>
2022-04-21 15:01:40 +1000

    Added main menu

---
+++ mq-game-jam/Assets/UIHover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float hoverSpeed = 3;
+    public float scaleMultiplier = 1.5f;
+
+    private Vector2 defaultScale;
+
+    private Vector2 scaleTo = Vector2.zero;
+
+    private RectTransform rect;
+
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        defaultScale = rect.sizeDelta;
+        scaleTo = defaultScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        scaleTo = defaultScale * scaleMultiplier;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        scaleTo = defaultScale;
+    }
+
+    void Update()
+    {
+        rect.sizeDelta = Vector2.MoveTowards(rect.rect.size, scaleTo, Time.deltaTime * hoverSpeed);
+    }
+}
